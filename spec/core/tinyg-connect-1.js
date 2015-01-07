f75934376c4746c7f1e2fdd278875c7f0e982c34
Rob Giseburt <giseburt@gmail.com>
2015-01-07 10:16:49 -0600

    Fixed glitch with response object matching. Added more tests.

--- spec/core/tinyg-connect-1.js
+++ spec/core/tinyg-connect-1.js
@@ -126,54 +126,66 @@ describe("Check firmware and hardware numbers for up to date values", function (
   it('Checks Hardwave Value @v8 @v9', function () {
     return g.get("hv").should.eventually.be.above(testData.min_hv);
 	});
+});
 
-  describe("Check Parameters", function () {
+describe("Check Parameters", function () {
 
-    // Set parameter tests
-    testData.setParameterTests.forEach(function(v) {
-      if (v.parameters === undefined){
-          v.parameters = [v.parameter];
+  // Set parameter tests
+  testData.setParameterTests.forEach(function(v) {
+    if (v.parameters === undefined){
+        v.parameters = [v.parameter];
+    }
+    if (v.status === undefined){
+      v.status = 0;
+    }
+
+    v.parameters.forEach(function(p) {
+      var description = 'Setting ' + p + ' to ' + util.inspect(v.value) + ' does what we expect @v8 @v9';
+      if (v.description) {
+        description = util.format(v.description, p);
       }
-      if (v.status === undefined){
-        v.status = 0;
-      }
-
-      v.parameters.forEach(function(p) {
-        it('Setting ' + p + ' to ' + v.value + ' does what we expect @v8 @v9', function () {
-          // set() returns a promise. If we put a function in the promise.catch(),
-          // it will give us a TinyGError object, with the full response object in
-          // the data member.
-          // It will only throw an error if the status is non-zero. If the status is
-          // zero, it will resolve with the value the TinyG sent back.
-          var promise = g.set(p, v.value).catch(function (e) {
-            // e.data contains our full response
-            if (e.data.f[0] !== (v.status || 0)) {
-              throw(new Error(util.format("Bad response, expected status (%d), got response: ", v.status, e.data)));
-            }
-          })
-
-          if (util.isArray(v.returns)) {
-            promise.should.eventually.be.within(v.returns);
-          } else if (v.exists === true) {
-            promise.should.eventually.exist;
-          } else if (v.returns === undefined || v.returns === null) {
-            promise.should.eventually.not.exist;
-          } else {
-            promise.should.eventually.equal(v.returns);
+      it(description, function () {
+        // set() returns a promise. If we put a function in the promise.catch(),
+        // it will give us a TinyGError object, with the full response object in
+        // the data member.
+        // It will only throw an error if the status is non-zero. If the status is
+        // zero, it will resolve with the value the TinyG sent back.
+        var promise = g.set(p, v.value).catch(function (e) {
+          // e.data contains our full response
+          if (e.data.f[0] !== (v.status || 0)) {
+            throw(new Error(util.format("Bad response, expected status (%d), got response: ", v.status, e.data)));
           }
+        })
 
-          return promise;
-        });
+        if (util.isArray(v.returns)) {
+          promise = promise.should.eventually.be.within(v.returns[0], v.returns[1]);
+        } else if (v.exists === true) {
+          promise = promise.should.eventually.exist;
+        } else if (v.returns === undefined || v.returns === null) {
+          // If we have a *Keys, then we actually implicitly check for exists.
+          if (!v.exactKeys && !v.exactKeys) {
+            promise = promise.should.eventually.not.exist;
+          }
+        } else {
+          promise = promise.should.eventually.eql(v.returns);
+        }
+
+        if (util.isArray(v.exactKeys)) {
+          promise = promise.should.eventually.have.keys(v.exactKeys);
+        } else if (util.isArray(v.hasKeys)) {
+          promise = promise.should.eventually.include.keys(v.hasKeys);
+        }
+
+        return promise;
       });
     });
-
   });
 
-  describe("Checks System Group Values", function () {
-    //Check all sys values are present
-    it('check all sys values are present @v8', function () {
-      return g.get("sys").should.eventually.contain.keys(testData.sys.propertyList);
-    });
-  });
+});
 
+describe("Checks System Group Values", function () {
+  //Check all sys values are present
+  it('check all sys values are present @v8', function () {
+    return g.get("sys").should.eventually.contain.keys(testData.sys.propertyList);
+  });
 });
