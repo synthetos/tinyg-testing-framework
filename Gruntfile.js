6563089fa1363da6e35b1c17ecbda5e2eb5e4362
Rob Giseburt <giseburt@gmail.com>
2015-05-01 09:33:24 -0500

    Temporarily locking tests to just arcs.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
           clearRequireCache: false 			// Optionally clear the require cache before running tests (defaults to false)
         },
 //        src: ['spec/**/tinyg-*.js']
-        src: ['tests/**/*.js']
+        src: ['tests/000-*.js', 'tests/005-*/*.js']
       }
     }
   });
